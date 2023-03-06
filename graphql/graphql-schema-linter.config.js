15575a2f583e1b68381c9ba0765af7794456e244
na2na-p <na2na@na2na.dev>
2023-03-06 13:57:06 +0900

    Implements some entities

--- graphql/graphql-schema-linter.config.js
+++ graphql/graphql-schema-linter.config.js
@@ -6,21 +6,21 @@ module.exports = {
 		'descriptions-are-capitalized',
 		'enum-values-all-caps',
 		'enum-values-have-descriptions',
-		'enum-values-sorted-alphabetically',
 		'fields-are-camel-cased',
 		'fields-have-descriptions',
-		'input-object-fields-sorted-alphabetically',
 		'input-object-values-are-camel-cased',
 		'input-object-values-have-descriptions',
 		'interface-fields-sorted-alphabetically',
 		'relay-connection-types-spec',
 		'relay-connection-arguments-spec',
-		'type-fields-sorted-alphabetically',
 		'types-are-capitalized',
 		'types-have-descriptions',
 	],
 	schemaPaths: ['./dist/schema.graphql'],
 	// NOTE: ホワイトリスト方式なのでignoreに追加しても無効にならない
 	// ignore: [
+	// 'enum-values-sorted-alphabetically',
+	// 'type-fields-sorted-alphabetically',
+	// 'input-object-fields-sorted-alphabetically'
 	// ]
 };
