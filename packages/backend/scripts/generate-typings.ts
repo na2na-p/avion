35c34e18d8603278c90c561a15b21ecd57cd8902
na2na-p <na2na@na2na.dev>
2023-04-17 23:03:10 +0900

    Add graphql base

---
+++ packages/backend/scripts/generate-typings.ts
@@ -0,0 +1,8 @@
+import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
+import { join } from 'path';
+
+const definitionsFactory = new GraphQLDefinitionsFactory();
+definitionsFactory.generate({
+  typePaths: [join(process.cwd(), '../graphql/dist/schema.graphql')],
+  path: join(process.cwd(), '../src/generated/types.ts'),
+});
--- packages/backend/src/app/app.module.ts
+++ packages/backend/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // NOTE: 本番ではfalseに
+      // playground: false,
+      path: '/api',
+      typePaths: [join(process.cwd(), '../graphql/dist/schema.graphql')],
+      definitions: {
+        path: join(process.cwd(), 'src/generated/types.ts'),
+      },
+    }),
+  ],
 })
 export class AppModule {}
