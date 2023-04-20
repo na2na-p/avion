55e22de0bb41bd395a396cf24e5f4868a74b3a38
na2na-p <na2na@na2na.dev>
2023-04-20 10:21:48 +0900

    モジュール群の整理

--- packages/backend/src/app/app.module.ts
+++ packages/backend/src/app/app.module.ts
@@ -1,23 +1,6 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { join } from 'path';
-
+import { GraphQLServerModule } from '@/graphql/graphql.module';
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      path: '/api',
-      typePaths: [join(process.cwd(), '../graphql/dist/schema.graphql')],
-      definitions: {
-        path: join(process.cwd(), 'src/generated/types.ts'),
-        emitTypenameField: true,
-      },
-      playground: false,
-      // TODO: productionであればfalseになるように
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
-    }),
-  ],
+  imports: [GraphQLServerModule],
 })
 export class AppModule {}
