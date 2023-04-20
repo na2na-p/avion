c41317d33115db3dd8a85e703829731bdd964007
na2na-p <na2na@na2na.dev>
2023-04-20 14:54:07 +0900

    Add generate options

--- packages/backend/scripts/generate-typings.ts
+++ packages/backend/scripts/generate-typings.ts
@@ -1,10 +1,23 @@
 import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
 import { join } from 'path';
+import yargs from 'yargs';
+
+const argv = yargs
+  .options({
+    watch: {
+      alias: 'w',
+      type: 'boolean',
+      description: 'watch mode',
+      default: false,
+    },
+  })
+  .help()
+  .parseSync();
 
 const definitionsFactory = new GraphQLDefinitionsFactory();
 definitionsFactory.generate({
   typePaths: [join(process.cwd(), '../graphql/dist/schema.graphql')],
   path: join(process.cwd(), './src/generated/types.ts'),
   emitTypenameField: true,
-  watch: true,
+  watch: argv.watch,
 });
